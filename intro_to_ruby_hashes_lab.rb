db1fc34445797213ae9b4d02bcde084e99f74790
Leonie Dong <leoniedong@hotmail.com>
2019-04-09 23:43:43 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -31,7 +31,7 @@ def monopoly_with_third_tier
 				:pennsylvania_railroad => {},
 				:b_and_o_railroad => {},
 				:shortline => {}
-			}
+			},
 			:rent_in_dollars => {
 				:one_piece_owned => 25,
 				:two_pieces_owned => 50,
@@ -51,7 +51,7 @@ def monopoly_with_fourth_tier
 				:pennsylvania_railroad => {mortgage_value: "$200"},
 				:b_and_o_railroad => {mortgage_value: "$400"},
 				:shortline => {mortgage_value: "$80"},
-			}
+			},
 			:rent_in_dollars => {
 				:one_piece_owned => 25,
 				:two_pieces_owned => 50,
